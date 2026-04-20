f5e5726f949ff226a59e83407b4a2921914d4b39
dmitri4222-collab <dmitri4222@gmail.com>
2026-04-20 15:08:46 +0300

    Update SingletonModule.kt

--- vector-app/src/main/java/im/vector/app/core/di/SingletonModule.kt
+++ vector-app/src/main/java/im/vector/app/core/di/SingletonModule.kt
@@ -72,8 +72,6 @@ import org.matrix.android.sdk.api.session.Session
 import org.matrix.android.sdk.api.session.sync.filter.SyncFilterParams
 import org.matrix.android.sdk.api.settings.LightweightSettingsStorage
 import javax.inject.Singleton
-import im.vector.app.core.services.DefaultGuardServiceStarter
-import im.vector.app.core.services.GuardServiceStarter
 
 @InstallIn(SingletonComponent::class) @Module abstract class VectorBindModule {
 
@@ -118,9 +116,6 @@ import im.vector.app.core.services.GuardServiceStarter
 
     @Binds
     abstract fun bindGetDeviceInfoUseCase(getDeviceInfoUseCase: DefaultGetDeviceInfoUseCase): GetDeviceInfoUseCase
-
-    @Binds
-    abstract fun bindGuardServiceStarter(starter: DefaultGuardServiceStarter): GuardServiceStarter
 }
 
 @InstallIn(SingletonComponent::class) @Module object VectorStaticModule {
