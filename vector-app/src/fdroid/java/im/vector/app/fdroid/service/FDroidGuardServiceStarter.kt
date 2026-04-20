ed221ea581b1a16feabe5d1146e1f9fb3287ed56
dmitri4222-collab <dmitri4222@gmail.com>
2026-04-20 15:11:14 +0300

    Update FDroidGuardServiceStarter.kt

--- vector-app/src/fdroid/java/im/vector/app/fdroid/service/FDroidGuardServiceStarter.kt
+++ vector-app/src/fdroid/java/im/vector/app/fdroid/service/FDroidGuardServiceStarter.kt
@@ -4,36 +4,59 @@
  * SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-Element-Commercial
  * Please see LICENSE files in the repository root for full details.
  */
-
 package im.vector.app.fdroid.service
 
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import androidx.core.content.ContextCompat
 import im.vector.app.core.services.GuardServiceStarter
+import im.vector.app.core.services.VectorSyncAndroidService
+import im.vector.app.features.settings.BackgroundSyncMode
 import im.vector.app.features.settings.VectorPreferences
+import org.matrix.android.sdk.api.Matrix
 import timber.log.Timber
 import javax.inject.Inject
 
 class FDroidGuardServiceStarter @Inject constructor(
         private val preferences: VectorPreferences,
-        private val appContext: Context
+        private val appContext: Context,
+        private val matrix: Matrix
 ) : GuardServiceStarter {
 
     override fun start() {
-        if (preferences.isBackgroundSyncEnabled()) {
-            try {
-                Timber.i("## Sync: starting GuardService")
-                val intent = Intent(appContext, GuardAndroidService::class.java)
-                ContextCompat.startForegroundService(appContext, intent)
-            } catch (ex: Throwable) {
-                Timber.e("## Sync: ERROR starting GuardService")
+        try {
+            Timber.i("## Sync: starting GuardService")
+            // Start no-op foreground service to prevent process kill
+            val guardIntent = Intent(appContext, GuardAndroidService::class.java)
+            ContextCompat.startForegroundService(appContext, guardIntent)
+
+            // Start sync service if background sync is enabled
+            if (preferences.isBackgroundSyncEnabled()) {
+                val session = matrix.authenticationService().getLastAuthenticatedSession() ?: return
+                val syncIntent = VectorSyncAndroidService.newPeriodicIntent(
+                    context = appContext,
+                    sessionId = session.sessionId,
+                    syncTimeoutSeconds = preferences.backgroundSyncTimeOut(),
+                    syncDelaySeconds = preferences.backgroundSyncDelay(),
+                    isNetworkBack = false
+                )
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    appContext.startForegroundService(syncIntent)
+                } else {
+                    appContext.startService(syncIntent)
+                }
             }
+        } catch (ex: Throwable) {
+            Timber.e(ex, "## Sync: ERROR starting services")
         }
     }
 
     override fun stop() {
-        val intent = Intent(appContext, GuardAndroidService::class.java)
-        appContext.stopService(intent)
+        val guardIntent = Intent(appContext, GuardAndroidService::class.java)
+        appContext.stopService(guardIntent)
+
+        val syncIntent = VectorSyncAndroidService.stopIntent(appContext)
+        appContext.startService(syncIntent)
     }
 }
