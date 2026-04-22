33e6beddfbfd3151fac1963b49e2144bf48da676
dmitri4222-collab <dmitri4222@gmail.com>
2026-04-23 00:34:24 +0300

    Update DebugModule.kt

--- vector-app/src/debug/java/im/vector/app/features/debug/di/DebugModule.kt
+++ vector-app/src/debug/java/im/vector/app/features/debug/di/DebugModule.kt
@@ -4,7 +4,6 @@
  * SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-Element-Commercial
  * Please see LICENSE files in the repository root for full details.
  */
-
 package im.vector.app.features.debug.di
 
 import android.content.Context
@@ -18,26 +17,27 @@ import im.vector.app.core.debug.DebugNavigator
 import im.vector.app.core.debug.DebugReceiver
 import im.vector.app.core.debug.LeakDetector
 import im.vector.app.features.debug.DebugMenuActivity
-import im.vector.app.leakcanary.LeakCanaryLeakDetector
 import im.vector.app.receivers.VectorDebugReceiver
+import javax.inject.Inject
 
 @InstallIn(SingletonComponent::class)
 @Module
 abstract class DebugModule {
 
     companion object {
-
         @Provides
         fun providesDebugNavigator() = object : DebugNavigator {
             override fun openDebugMenu(context: Context) {
                 context.startActivity(Intent(context, DebugMenuActivity::class.java))
             }
         }
+
+        @Provides
+        fun providesLeakDetector(): LeakDetector = object : LeakDetector {
+            override fun enable(enable: Boolean) = Unit
+        }
     }
 
     @Binds
     abstract fun bindsDebugReceiver(receiver: VectorDebugReceiver): DebugReceiver
-
-    @Binds
-    abstract fun bindsLeakDetector(leakDetector: LeakCanaryLeakDetector): LeakDetector
 }
